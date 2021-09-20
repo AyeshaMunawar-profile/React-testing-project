da739514810742367a538c0bad2ebf0e7e94959a
Ayesha Munawar <ayeshamunawarofficial@gmail.com>
2021-09-20 14:02:42 +0500

    RTP> Add a sample header component
    RTP> Add a sample header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react'
-import Greeting from './container/Greeting/Greeting'
 import './App.css'
-import Sum from './components/Sum/Sum'
+import HomePage from './container/HomePage/HomePage'
+import './App.scss'
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<Greeting />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-				<Sum />
-			</header>
+			<HomePage />
 		</div>
 	)
 }
---
+++ src/container/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+
+const HomePage = () => (
+	<>
+		<div className='home-page'>
+			<Header name='hello' />
+		</div>
+	</>
+)
+export default HomePage
